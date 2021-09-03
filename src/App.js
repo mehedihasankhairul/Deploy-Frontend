060f6028fe205888f326c70c38008fa524b5e5b3
MeHeDi HaSaN Khairul <mhkhairul78@gmail.com>
2021-09-03 12:01:57 +0600

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>hello world</h1>
     </div>
   );
 }
