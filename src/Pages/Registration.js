f30f46c34753e75e358a8a0055ddb15437e94aef
hadiuldev <hadiulislamofficial@gmail.com>
2021-09-11 09:36:40 +0600

    setup log-signup ui

---
+++ src/Pages/Registration.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomerRegistration from '../Components/CustomerAuth/CustomerRegistration/CustomerRegistration';
+
+const Registration = () => {
+    return (
+        <>
+            <CustomerRegistration />
+        </>
+    );
+};
+
+export default Registration;
