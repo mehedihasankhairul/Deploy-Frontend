c44841ed569202ed223e00f69c20e0d86de92e54
hadiuldev <hadiulislamofficial@gmail.com>
2021-09-12 03:55:18 +0600

    fix product slider

--- src/Components/CategoriesSlider/MidCategoriesSlider/MidCategoriesSlider.js
+++ src/Components/CategoriesSlider/MidCategoriesSlider/MidCategoriesSlider.js
@@ -3,54 +3,45 @@ import { useSelector } from 'react-redux';
 // import { Link } from 'react-router-dom';
 // import Error from '../../../Pages/Error/Error';
 
-import Slider from 'react-slick';
 import MidItem from '../../MidItem';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 
 const MidCategoriesSlider = () => {
   const { midCategory } = useSelector((state) => state.category);
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 5,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 450,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 5
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 2
+  }
+};
+
+
 
   return (
-    <div className="endCategoriesSlider">
+    <div className="midCategoriesSlider py-4">
       <h3 className="heading">Mid Categories</h3>
-      <Slider {...settings} className="endcg-slider">
+      <Carousel responsive={responsive}>
         {midCategory.slice(0, 5).map((item) => (
           <MidItem key={item.id} item={item} />
         ))}
-      </Slider>
+      </Carousel>
     </div>
   );
 };
