44bde17a7cde2bcefc61d7d61cdc87b98ccad8a6
Ishtiak <iahmed1606@gmail.com>
2021-09-13 19:53:13 +0600

    navbar added on other pages

--- src/Pages/ProductByCat/ProductByCat.jsx
+++ src/Pages/ProductByCat/ProductByCat.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
+import Navbar from '../../Components/Shared/Navbar/Navbar';
 
 const baseUrl = 'https://deploy.com.bd/assets/uploads/';
 
@@ -17,36 +18,40 @@ export default function ProductByCat() {
     (product) => product.ecatId === selectedCategory.id
   );
   return (
-    <div>
-      {/* <Navigator /> */}
-      <span className="navigator">
-        <Link to="/">Home</Link>
-        {'> '} <Link to={'/' + category}>{category.replaceAll('-', ' ')}</Link>
-        {'> '}{' '}
-        <Link to={'/' + category + '/' + mid_category}>
-          {mid_category.replaceAll('-', ' ')}
-        </Link>
-        {'> '} <span>{end_category.replaceAll('-', ' ')}</span>
-      </span>
-      <div className="product_list">
-        {filterProducts.length
-          ? filterProducts.map((prod) => (
-              <div key={prod.id} className="product_item">
-                <img src={baseUrl + prod.image} alt="" />
-                <p>{prod.productName}</p>
-                <p>BDT {prod.currentPrice}</p>
-                <button>Add To Cart</button>
-              </div>
-            ))
-          : allProducts.map((prod) => (
-              <div key={prod.id} className="product_item">
-                <img src={baseUrl + prod.image} alt="" />
-                <p>{prod.productName}</p>
-                <p>BDT {prod.currentPrice}</p>
-                <button>Add To Cart</button>
-              </div>
-            ))}
+    <>
+      <Navbar />
+      <div>
+        {/* <Navigator /> */}
+        <span className="navigator">
+          <Link to="/">Home</Link>
+          {'> '}{' '}
+          <Link to={'/' + category}>{category.replaceAll('-', ' ')}</Link>
+          {'> '}{' '}
+          <Link to={'/' + category + '/' + mid_category}>
+            {mid_category.replaceAll('-', ' ')}
+          </Link>
+          {'> '} <span>{end_category.replaceAll('-', ' ')}</span>
+        </span>
+        <div className="product_list">
+          {filterProducts.length
+            ? filterProducts.map((prod) => (
+                <div key={prod.id} className="product_item">
+                  <img src={baseUrl + prod.image} alt="" />
+                  <p>{prod.productName}</p>
+                  <p>BDT {prod.currentPrice}</p>
+                  <button>Add To Cart</button>
+                </div>
+              ))
+            : allProducts.map((prod) => (
+                <div key={prod.id} className="product_item">
+                  <img src={baseUrl + prod.image} alt="" />
+                  <p>{prod.productName}</p>
+                  <p>BDT {prod.currentPrice}</p>
+                  <button>Add To Cart</button>
+                </div>
+              ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
