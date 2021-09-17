b5ebde616f7afe4f23cfc7765992b05199de886b
MeHeDi HaSaN Khairul <mhkhairul78@gmail.com>
2021-09-17 18:13:34 +0600

    style solve

--- src/Components/CustomerAuth/CustomerLogin/CustomerLogin.js
+++ src/Components/CustomerAuth/CustomerLogin/CustomerLogin.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import logo from '../../../assets/Images/deoloy-logo.jpeg';
 
 const CustomerLogin = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+
+  useEffect(() => {
+    if (localStorage.getItem('user-info')) {
+      history.push("/cart")
+    }
+  }, [])
+
+  const login = (e) => {
+    fetch(`https://frozen-caverns-97537.herokuapp.com/api/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: userName,
+        password: password,
+      }),
+    })
+      .then(res => res.json())
+      .then(data => {
+        sessionStorage.setItem("JwtToken", data.jwt);
+      })
+      // .then(() => {
+      //     window.location.reload();
+      // })
+      .then(() => {
+        // history.replace(from);
+        // window.location.reload();
+      })
+      .catch(error => console.log('error', error));
+    e.preventDefault()
+  }
+
   return (
     <div>
       <div className="bg-secondary text-light d-flex aligin-items-center justify-content-center py-5">
@@ -17,31 +54,33 @@ const CustomerLogin = () => {
           <div className="logo d-flex justify-content-center">
             <img className="w-50" src={logo} alt="logo" />
           </div>
-          <form>
-            <div className="mb-3">
-              <label className="form-label">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-            <button type="submit" className="btn w-100 btn-success">
-              Submit
-            </button>
-            <p className="mt-4">
-              New to Deploy? <Link to="/registration">Sign up Now</Link>
-            </p>
-          </form>
+          <div className="mb-3">
+            <label className="form-label">User Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={(e) => setUserName(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <p className="mt-4">
+            New to Deploy? <Link to="/registration">Sign up Now</Link>
+          </p>
+          <button onClick={login}
+            className="btn w-100 btn-success">
+            Submit
+          </button>
+
         </div>
       </div>
     </div>
